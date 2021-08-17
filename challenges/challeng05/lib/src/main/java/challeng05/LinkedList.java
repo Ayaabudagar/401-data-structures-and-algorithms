5509994cb8667a2d7615e2b5814ba2bda3a47cfa
Ayaabudagar <ayakhlil23@yahoo.com>
2021-08-17 20:53:35 +0300

    done

--- challenges/challeng05/lib/src/main/java/challeng05/LinkedList.java
+++ challenges/challeng05/lib/src/main/java/challeng05/LinkedList.java
@@ -34,4 +34,51 @@ public class LinkedList {
         return result;
     }
 
+    public void append(int value)  {
+        Node newInsertNode = new Node(value);
+        if (head == null) {
+            head = newInsertNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newInsertNode;
+        }
+    }
+
+    public void insertBefore(int value, int newValue) {
+        Node newInsertNode = new Node(newValue);
+        if (head == null) {
+            head = newInsertNode;
+        } else if (head.value == value) {
+            insert(newValue);
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                if (current.next.value == value) {
+                    newInsertNode.next = current.next;
+                    current.next = newInsertNode;
+                    break;
+                }
+                current = current.next;
+            }
+        }
+    }
+
+    public void insertAfter(int value, int newValue) {
+        Node newInsertNode = new Node(newValue);
+        if (head == null) {
+            head = newInsertNode;
+        } else {
+            Node current = head;
+            while (current != null) {
+                if (current.value == value) {
+                    newInsertNode.next = current.next;
+                    current.next = newInsertNode;
+                }
+                current = current.next;
+            }
+        }
+    }
 }

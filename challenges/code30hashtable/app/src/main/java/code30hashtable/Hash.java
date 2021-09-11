9ce988f9e880404182e12ec43e32b11cee40cd60
Ayaabudagar <ayakhlil23@yahoo.com>
2021-09-11 23:02:53 +0300

    done

--- challenges/code30hashtable/app/src/main/java/code30hashtable/Hash.java
+++ challenges/code30hashtable/app/src/main/java/code30hashtable/Hash.java
@@ -10,4 +10,28 @@ public class Hash {
         this.key = key;
         this.value = value;
     }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public Hash getNext() {
+        return next;
+    }
+
+    public void setNext(Hash next) {
+        this.next = next;
+    }
 }

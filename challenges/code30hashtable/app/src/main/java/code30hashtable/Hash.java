ac376a3c4a24bbd4db6881747602f343de1c39c0
Ayaabudagar <ayakhlil23@yahoo.com>
2021-09-11 23:01:30 +0300

    done

---
+++ challenges/code30hashtable/app/src/main/java/code30hashtable/Hash.java
@@ -0,0 +1,13 @@
+package code30hashtable;
+
+public class Hash {
+    public String key;
+    public int value;
+    public Hash next;
+
+
+    public Hash(String key, int value) {
+        this.key = key;
+        this.value = value;
+    }
+}
---
+++ challenges/code30hashtable/app/src/main/java/code30hashtable/Hashtable.java
@@ -0,0 +1,65 @@
+package code30hashtable;
+
+import java.util.LinkedList;
+
+public class Hashtable {
+//    LinkedList<Object>[] arr;
+//
+//    public void HashTable(int size) {
+//        this.arr= new LinkedList[size];
+//
+//        for(int j = 0; j < this.arr.length; ++j) {
+//            this.arr[j] = new LinkedList();
+//        }
+//
+//    }
+//
+
+public Hash[] array =new Hash[20];
+    public int hash(String key){
+        int k=0;
+        int result=0;
+        for (int i = 0; i < key.length(); i++) {
+            k= k+(int)key.charAt(i);
+            result=Math.round(k/29);
+        }
+
+        return result;
+    }
+
+    public void add(String key, int value){
+        if(array[hash(key)] ==null){
+            array[hash(key)]=new Hash(key,value);
+        }else {
+            Hash newValue=new Hash(key,value);
+            array[hash(key)].next=newValue;
+        }
+
+    }
+
+
+    public boolean contains(String key){
+        Hash current = array[hash(key)];
+
+        while (current!= null){
+            if(current.key==key){
+                 return true;
+            }
+            current=current.next;
+
+        }
+        return false;
+    }
+    public int get(String NewKey){
+        Hash current = array[hash(NewKey)];
+        int T= 0;
+        while (current!= null){
+            if(current.key==NewKey){
+                T=current.value;
+            }
+            current=current.next;
+
+        }
+        return T;
+    }
+}

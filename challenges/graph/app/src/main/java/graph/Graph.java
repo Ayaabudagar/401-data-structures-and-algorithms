e2bb0569cc2badd53a7bac2c690866f96353f061
Ayaabudagar <ayakhlil23@yahoo.com>
2021-09-19 18:16:13 +0300

    done

--- challenges/graph/app/src/main/java/graph/Graph.java
+++ challenges/graph/app/src/main/java/graph/Graph.java
@@ -2,100 +2,79 @@ package graph;
 
 import java.util.*;
 
-public class Graph <T>{
-    private Map<Node, List<Node>> map = new HashMap<>();
-    public Node<T> addVertex(T value ){
+public class Graph {
 
-        Node vertex = new Node( value);
-        map.put(vertex,new ArrayList<>());
+    private Map<Node, List<Node>> adjVertices = new HashMap<>();
+
+    public void setAdjVertices(Map<Node, List<Node>> adjVertices) {
+        this.adjVertices = adjVertices;
+    }
+
+
+
+    public List<Node> addEdge(String label1, String label2) {
+        Node v1 = new Node(label1);
+        Node v2 = new Node(label2);
+        adjVertices.get(v1).add(v2);
+        adjVertices.get(v2).add(v1);
+        return adjVertices.get(v1);
+    }
+    public Node addVertex(String label) {
+        Node vertex= new Node(label);
+        adjVertices.putIfAbsent(vertex, new ArrayList<>());
         return vertex;
-
-    }
-//    Map<Node <T>,
-//            ArrayList<Node<T>>> map = new HashMap<>();
-    public void addEdge(Node node1 , Node node2){
-        map.get(node1).add(node2);
-        map.get(node2).add(node1);
     }
 
-    public ArrayList<Node> getNodes(){
-
-        if (map.isEmpty()){
-            return null;
-        }
-
-        ArrayList<Node> list = new ArrayList<>();
-        list.addAll(map.keySet());
-        return list;
-
-    }
-
-    public List<Node> getNeighbors(Node node){
-        return map.get(node);
-    }
 
     public int getSize(){
-        return map.size();
+//        System.out.println(adjVertices.size());
+        return adjVertices.size();
+    }
+    public Map<Node, List<Node>> getAdjVertices() {
+        return adjVertices;
     }
 
-    public Node<T> addNode(String a) {
-        return null;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    public List<Node> getNeighbors(Node vertex){
+        return (List<Node>) adjVertices.get(vertex);
     }
-    public ArrayList<Node> breadthFirst( Node vertex){
-        ArrayList<Node> array  =new ArrayList<>();
-        ArrayList<Node> vertexArray = new ArrayList<>();
-        List<Node> allNode = map.get(vertexArray.get(0));
-        vertexArray.add(vertex);
-        for(Node vertex2: allNode){
-            if(!array.contains(vertex2)){
-                vertexArray.add(vertex2);
-        while (vertexArray.size() != 0){
-            array.add(vertexArray.get(0));
 
-            vertexArray.remove(0);
 
+    public List<Node> breadthFirst( Node vertex){
+        List<Node> visited  =new ArrayList<>();
+        List<Node> listOfVertex = new ArrayList<>();
+
+        listOfVertex.add(vertex);
+
+        while (listOfVertex.size() != 0){
+            visited.add(listOfVertex.get(0));
+            List<Node> allNeighbors = adjVertices.get(listOfVertex.get(0));
+            listOfVertex.remove(0);
+            System.out.println("allNeighbors: "+allNeighbors);
+            for(Node vertex2: allNeighbors){
+                if(!visited.contains(vertex2)){
+                    listOfVertex.add(vertex2);
+                    System.out.println(vertex2);
+                }
             }
         }
-
+        return visited;
     }
-        return array;
 
-}
+    public void addEdge(String label1, String label2, int weight) {
+        Node wVertex1 = new Node(label1, weight);
+        Node wVertex2 = new Node(label2, weight);
+
+        Node vertex1 = new Node(label1, weight);
+        Node vertex2 = new Node(label2, weight);
+
+        adjVertices.get(vertex1).add(wVertex2);
+        adjVertices.get(vertex2).add(wVertex1);
+    }
 
 
-
-    private void tripPrice(String city1, String city2, Graph graph){
-        Node vertex= new Node(city1);
-        if (graph.getNeighbors(vertex) == null){
-            return;
-        }
-
-        for (Node vertex1: graph.getNeighbors(vertex)) {
-            if (Objects.equals(city2, vertex1)){
-                cost ++;
-                break;
-            }
-
-        }
-
-}
-     int cost = 0;
-    public int citiesTrip(Graph graph, ArrayList<String> cities){
-        cost = 0;
+    private int totalCost = 0;
+    public int citiesTrip(Graph graph, List<String> cities){
+        totalCost = 0;
         if (cities.size() <= 1  ){
             return 0;
         }
@@ -103,10 +82,42 @@ public class Graph <T>{
             return 0;
         }
         for (int i = 0; i < cities.size()-1; i ++){
-            tripPrice(cities.get(i), cities.get(i + 1), graph);
+            calculatePrice(cities.get(i), cities.get(i + 1), graph);
         }
 
-        return cost;
+        return totalCost;
     }
 
+    private void calculatePrice(String city1, String city2, Graph graph){
+        Node vertex= new Node(city1);
+        if (graph.getNeighbors(vertex) == null){
+            return;
+        }
+
+        for (Node vertex1: graph.getNeighbors(vertex)) {
+            if (Objects.equals(city2, vertex1.label)){
+                totalCost += vertex1.weight;
+                break;
+            }
+        }
+    }
+
+    List list= new ArrayList();
+    public List<Node> depthFirst(Node vertex){
+        if(vertex == null){
+            return list;
+        }
+        list.add(vertex.label);
+        System.out.println(vertex);
+        List<Node> neighborsList = getNeighbors(vertex);
+        if (neighborsList.size()==0){
+            return list;
+        }
+        for ( Node v: neighborsList) {
+            if(!list.contains(v.label))
+                depthFirst(v);
+        }
+
+        return list;
+    }
 }
--- challenges/graph/app/src/main/java/graph/Node.java
+++ challenges/graph/app/src/main/java/graph/Node.java
@@ -1,9 +1,37 @@
 package graph;
 
-public class Node <T>  {
-    T value;
+import java.util.Objects;
 
-    public Node(T value) {
-        this.value = value;
+public class Node {
+    String label;
+    public int weight;
+
+    Node(String label){
+        this.label = label;
+    }
+
+    Node(String label, int weight){
+        this.label = label;
+        this.weight = weight;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Node vertex = (Node) o;
+        return Objects.equals(label, vertex.label);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(label);
+    }
+
+    @Override
+    public String toString() {
+        return "Vertex{" +
+                "label='" + label + '\'' +
+                '}';
     }
 }
